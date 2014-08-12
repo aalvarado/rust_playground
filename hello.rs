e880bc79983d3e1c8e1fe77c529a04b78b6eb941
aalvarado <adan.alvarado7@gmail.com>
2014-08-11 23:36:25 -0700

    Play with pointers

--- hello.rs
+++ hello.rs
@@ -8,4 +8,12 @@ fn main() {
     let p = Person { name: "Foo".to_string(), age: 20 };
     println!("{:s}", p.name);
     println!("{:d}", p.age);
+    foo()
+}
+
+fn foo() {
+    let x = 10i;
+    let y = &x;
+
+    println!("{:p}", y);
 }
