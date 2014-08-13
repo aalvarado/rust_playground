66ffd0165bd76ae5a79d6391282bf5a7fec8545d
aalvarado <adan.alvarado7@gmail.com>
2014-08-12 23:54:23 -0700

    Trying out some concurrency?

--- concurrent.rs
+++ concurrent.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 fn main() {
     let numbers = vec![1i, 2i, 3i];
 
@@ -8,4 +10,21 @@ fn main() {
         let numbers = rx.recv();
         println!("{}", numbers[0]);
     });
+
+    main2();
+}
+
+fn main2() {
+    let numbers = vec![1i, 2i, 3i];
+    let numbers = Arc::new(numbers);
+
+    for num in range(0u, 3) {
+        let (tx, rx) = channel();
+        tx.send(numbers.clone());
+
+        spawn(proc(){
+            let numbers = rx.recv();
+            println!("{:d}", (*numbers)[num as uint]);
+        })
+    }
 }
